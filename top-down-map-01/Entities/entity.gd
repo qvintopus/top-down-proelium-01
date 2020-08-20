1209fbb4ba419d51b23e5e5b22a77dbb3c68c90f
mrinsomnia <martins.aldins@gmail.com>
2020-08-20 22:43:39 +0300

    Added simple peon with simple ideas

---
+++ top-down-map-01/Entities/entity.gd
@@ -0,0 +1,72 @@
+extends KinematicBody2D
+
+class_name Entity
+
+# moves to target
+# job list of actions with priorities
+# 
+
+const DEBUG_MODE:bool = false
+
+enum {IDLE, MOVE, HARVEST_WOOD, GATHER, PATROL}
+
+var targetPosition:Vector2 = Vector2.ZERO
+var speed:float = 5 *60
+var path: PoolVector2Array
+var state: = IDLE
+var Nav: Navigation2D
+var velocity: = Vector2.ZERO
+var line2D:Line2D
+
+
+
+func _ready():
+	Nav = get_tree().get_nodes_in_group("Navigation")[0]
+	line2D = $Line2D
+	line2D.set_as_toplevel(true)
+	
+
+func _physics_process(delta):
+	# find target
+	# move to target
+	match state:
+		MOVE:
+#			print("click")
+			state_move(delta)
+	if DEBUG_MODE:
+		update()
+
+func _draw():
+	if DEBUG_MODE:
+		draw_line(Vector2.ZERO, get_local_mouse_position(), Color.black)
+		
+
+
+func state_move(delta: float):
+	if path.size() > 0:
+		var d: float = position.distance_to(path[0])
+		if d > speed * delta:
+			var dir: Vector2 = (path[0] - position).normalized()
+			velocity = dir * speed
+			move_and_slide(velocity)
+		else:
+			if path.size() == 1:
+				velocity = (path[0] - position) / delta
+				move_and_slide(velocity)
+				state = IDLE
+			else:
+				path.remove(0)
+	
+
+func get_target_path(_targetPos):
+	targetPosition = _targetPos
+	path = Nav.get_simple_path(global_position, targetPosition, false)
+	if DEBUG_MODE:
+		line2D.points = path
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == 1:
+			state = MOVE
+			get_target_path(get_global_mouse_position())
+
--- top-down-map-01/Player.gd
+++ top-down-map-01/Player.gd
@@ -8,10 +8,14 @@ var speed:float = 10 *60
 
 
 
-
 func _process(delta):
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	position += direction * speed * delta
+
+
+
+
+
 
 
